86993d480d5aa894aeebe91020629119526630b3
Marlize Ramos Batista <marlize.batista@dbserver.com.br>
2021-04-04 22:57:10 -0300

    Splash screen e login

---
+++ ProjetoIHC/app/src/main/java/com/example/projetoihc/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.projetoihc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.projetoihc.ui.login.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ ProjetoIHC/app/src/main/java/com/example/projetoihc/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.example.projetoihc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.projetoihc.ui.login.LoginActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Handler handle = new Handler();
+        handle.postDelayed(new Runnable() {
+            @Override public void run() {
+                mostrarLogin();
+            }
+        }, 2000);
+    }
+
+    private void mostrarLogin() {
+        Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
